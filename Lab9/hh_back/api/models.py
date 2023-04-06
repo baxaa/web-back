a4d56815de271f737f74319b4401595f9137d4db
baxaa <onalbekovbakbergen7@gmail.com>
2023-04-06 14:57:13 +0600

    create some views, urls and e.c.

--- Lab9/hh_back/api/models.py
+++ Lab9/hh_back/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=10000)
+    city = models.CharField
+    address = models.TextField(max_length=500)
+
+    def to_json(self):
+        return {
+            'name': self.name,
+            'description' : self.description,
+            'city' : self.city,
+            'address' : self.address
+        }
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=10000)
+    salary = models.FloatField
+    company = models.ForeignKey(Company,on_delete=models.CASCADE,default = 0)
+
+    def to_json(self):
+        return {
+            'name': self.name,
+            'description':self.description,
+            'salary' : self.salary,
+            'company' : self.company.name
+        }
+
+
--- Lab9/hh_back/api/views.py
+++ Lab9/hh_back/api/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+import json
+from django.http.response import HttpResponse, JsonResponse
+from datetime import datetime, timedelta
+import requests
 
-# Create your views here.
+from api.models import Company, Vacancy
+
+
+
+def list_of_companies(request):
+    companies = []
+    print(companies)
+    for company in Company.objects.all():
+        companies.append(company.to_json())
+    print(companies)
+    return JsonResponse(companies,safe=False,json_dumps_params={'indent':2})
+
+def company_detail(request,company_id):
+    companies = []
+    for company in Company.objects.all():
+        companies.append(company.to_json())
+    for company in companies:
+        if company['id'] == company_id:
+            return JsonResponse(company,safe=False,json_dumps_params={'indent':2})
+    return JsonResponse({'error':'Company not found'})
+
+
+def list_of_vacancies(request):
+    vacancies = []
+    for vacancy in Vacancy.objects.all():
+        vacancies.append(vacancy.to_json())
+    return JsonResponse(vacancies,safe=False,json_dumps_params={'indent':2})
+
+def vacancy_detail(request,vacancy_id):
+    vacancies = []
+    for vacancy in Vacancy.objects.all():
+        vacancies.append(vacancy.to_json())
+    for vacancy in vacancies:
+        if vacancy['id'] == vacancy_id:
+            return JsonResponse(vacancy,safe=False,json_dumps_params={'indent':2})
+    return JsonResponse({'error':'Vacancy not found'})
+
+def vacancies_of_company(request,company_id):
+    vacancies = []
+    for vacancy in Vacancy.objects.all():
+        vacancies.append(vacancy.to_json)
+
+    matching_vacancies = list(filter(lambda x: x.company == company_id,vacancies))
+
+    if matching_vacancies.count != 0:
+        return JsonResponse(matching_vacancies,safe=False,json_dumps_params={'indent':2})
+    return JsonResponse({'error':'Vacancies not found'})
+
+def top_ten_vacancies(request):
+    vacancies = []
+    for vacancy in Vacancy.objects.all():
+        vacancies.append(vacancy.to_json)
+    vacancies.sort()
+    vacancies.reverse()
+    if(vacancies.count != 0):
+        return JsonResponse(vacancies[0:11],safe=False,json_dumps_params={'indent':2})
+    return JsonResponse({'error':'Error404'})
